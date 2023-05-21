ee03950f553cfeca50c1ad5fa4f07a1fa54a94a3
yuqi-wang-forever <1915780288@qq.com>
2023-05-21 23:28:00 +0800

    normalize URL and test

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(urlString){
+    const urlObj = new URL(urlString)
+    const hostpath = `${urlObj.hostname}${urlObj.pathname}`
+    if(hostpath.length > 0 && hostpath.slice(-1) === '/'){
+        return hostpath.slice(0,-1)
+    }
+    return hostpath
+     
+}        
+module.exports =  {
+    normalizeURL
+} 
---
+++ crawl.test.js
@@ -0,0 +1,28 @@
+const {normalizeURL} = require('./crawl')
+const {test,expect} = require('@jest/globals')
+
+test('normalizeURL strip protocol',()=>{
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL trailing slash',()=>{
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL strip http',()=>{
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL capital',()=>{
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
